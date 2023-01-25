1bb0b04b9d0bf1f6ebb36cc984e62b4032067500
Abhishek <asharma70420@gmail.com>
2023-01-25 21:47:37 +0530

    'python-code-to-make-a-heart'

---
+++ python code to make a heart.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(0, 2 * np.pi, 100)
+x = 16 * np.sin(t)**3
+y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2*np.cos(3*t) - np.cos(4*t)
+
+plt.plot(x, y, 'r-', linewidth=2)
+plt.show()
